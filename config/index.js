8d3eb9573014c1dd46a1df28100aaf5ffc38b7de
Andrianina RABARIVELO <andrianina.rabarivelo@gmail.com>
2019-12-02 12:01:38 +0300

    cron deleted

--- config/index.js
+++ config/index.js
@@ -1,14 +1,6 @@
 const fs = require('fs')
 
 module.exports = {
-  cron: {
-    seconds: '*/30',
-    minutes: '*',
-    hour: '*', //Tous les 24heures
-    dayOfMonth: '*',
-    month: '*',
-    dayOfWeek: '*',
-  },
   hubspot: {
     contactRequest: {
       count: 100 // Nombre de contact à prendre par requete

2183911b82ab58c07dc682329dfa6fb2720e0d12
Andrianina RABARIVELO <andrianina.rabarivelo@gmail.com>
2019-12-02 09:42:27 +0300

    route contact/getAll

--- routes/api/contact.js
+++ routes/api/contact.js
@@ -4,8 +4,7 @@ const Bigquery = require('../../services/bigquery')
 const Hubspot = require('../../services/hubspot')
 const bigqueryClient = Bigquery.getClient()
 const hubspot = Hubspot.getClient()
-
-router.get('/', function (req, res, next) {
+router.get('/getAll', function (req, res, next) {
 
   var contact = hubspot.forms.getSubmissions('4dcef6e8-394a-4dde-a688-16eb384765cd')
   // console.log(contact);
@@ -20,77 +19,4 @@ router.get('/', function (req, res, next) {
   })
 });
 
-router.get('/task/run', function (req, res, next) {
-  try {
-    task.run()
-    res.send("Task running")
-  } catch (err) {
-    console.log(err)
-    res.send("failed")
-  }
-})
-
-router.get('/connection', function (req, res, next) {
-
-  // Instantiates a client. Explicitly use service account credentials by
-  // specifying the private key file. All clients in google-cloud-node have this
-  // helper, see https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
-  async function createDataset() {
-    // Create the dataset
-    const [dataset] = await bigqueryClient.createDataset('BDD_test');
-    console.log(`Dataset ${dataset.id} created.`);
-  }
-  createDataset();
-
-});
-
-router.get('/bigquery/datasets/:dataset/tables/create/:table', async (req, res, next) => {
-  const dataset = bigqueryClient.dataset(req.params.dataset);
-
-  const table = dataset.table(req.params.table)
-  table.create((err, table, apiResponse) => {
-    console.log(`TABLE ${req.params.table} created`)
-  });
-});
-
-router.get('/bigquery/datasets/:dataset/tables/insert/:table', async (req, res, next) => {
-  const dataset = bigqueryClient.dataset(req.params.dataset);
-  const table = dataset.table(req.params.table);
-
-  const data = [
-    {
-      lastname: "randria",
-      firstname: "kevin",
-    },
-    {
-      lastname: "rabary",
-      firstname: "andria",
-    },
-    {
-      lastname: "rabary",
-      firstname: "felana",
-    },
-    {
-      lastname: "razaka",
-      firstname: "phil",
-    },
-  ]
-
-  table.insert(data, (err, apiResponse) => {
-    if (err) {
-      console.log(`Error when inserting data to table. ${err}`)
-    }
-    else {
-      console.log(`Data inserted`)
-    }
-  })
-
-});
-
-function checkTableMetadata(table) {
-  table.getMetadata((err, metadata, apiResponse) => {
-    console.log(metadata);
-    return !!metadata;
-  })
-}
 module.exports = router;

a85e14b2459f37a3a5f310ad2d1b5c3c3fdd6614
Kevin Randriajaoson <randriajaoson.kevin@outlook.com>
2019-12-03 15:48:58 +0300

    Change migrate VERB to POST

--- routes/api/contact.js
+++ routes/api/contact.js
@@ -4,7 +4,7 @@ const Bigquery = require('../../services/bigquery')
 const Hubspot = require('../../services/hubspot')
 const bigqueryClient = Bigquery.getClient()
 const hubspot = Hubspot.getClient()
-router.get('/migrate', async function (req, res, next) {
+router.post('/migrate', async function (req, res, next) {
   try {
     task.run();
     res.json({
